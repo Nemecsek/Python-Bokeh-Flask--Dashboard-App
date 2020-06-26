2d7d90f869f75703c2081ad0d295c887c8ff83cf
Alex Poca <alepoc69@gmail.com>
2020-06-26 12:29:34 +0200

    Local JS/CSS are now local; added "All Classes" selection

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-################################################################################
-#                             Titanic - Dashboard                              #
-# ---------------------------------------------------------------------------- #
-# Author:  Dario Radecic                                                       #
-# Version: 1.0                                                                 #
-################################################################################
+########################################################################################################################
+#                             Titanic - Dashboard
+# Version: 1.0  Dario Radecic
+#          1.1  Nemecsek
+# https://towardsdatascience.com/https-medium-com-radecicdario-next-level-data-visualization-dashboard-app-with-bokeh-flask-c588c9398f98
+# pip install numpy pandas bokeh==1.2.0 flask
 
-################################################################################
-#                                  > IMPORTS                                   #
-################################################################################
+########################################################################################################################
+#                                  > IMPORTS
+
 import math
 import numpy as np
 import pandas as pd
@@ -20,17 +20,12 @@ from bokeh.transform import factor_cmap
 
 from flask import Flask, render_template, request
 
-df = pd.read_csv('../data/titanic.csv')
+df = pd.read_csv('data/titanic.csv')
 df['Title'] = df['Name'].apply(lambda x: x.split(',')[1].strip().split(' ')[0])
 
-################################################################################
-#                             - END OF IMPORTS -                               #
-################################################################################
+########################################################################################################################
+#                               > CONSTANT VALUES
 
-
-################################################################################
-#                               > CONSTANT VALUES                              #
-################################################################################
 palette = ['#ba32a0', '#f85479', '#f8c260', '#00c2ba']
 
 chart_font = 'Helvetica'
@@ -42,14 +37,9 @@ default_padding = 30
 chart_inner_left_padding = 0.015
 chart_font_style_title = 'bold italic'
 
-################################################################################
-#                          - END OF CONSTANT VALUES -                          #
-################################################################################
+########################################################################################################################
+#                             > HELPER FUNCTIONS
 
-
-################################################################################
-#                             > HELPER FUNCTIONS                               #
-################################################################################
 def palette_generator(length, palette):
     int_div = length // len(palette)
     remainder = length % len(palette)
@@ -77,34 +67,38 @@ def plot_styler(p):
     p.toolbar_location = None
 
 
-def redraw(p_class):
-    survived_chart = survived_bar_chart(df, p_class)
-    title_chart = class_titles_bar_chart(df, p_class)
-    hist_age = age_hist(df, p_class)
+def redraw(selected_class):
+
+    selected_class = int(selected_class)
+    if selected_class == 0: # all classes
+        dataset = df
+    else: # single class
+        dataset = df[df['Pclass'] == selected_class]
+
+    class_texts = ["All Classes", "1st Class", "2nd Classes", "3rd Class"]
+    class_text = class_texts[selected_class]
+    survived_chart = survived_bar_chart(dataset, "Survival for " + class_text)
+    title_chart = class_titles_bar_chart(dataset, "Titles for " + class_text)
+    hist_age = age_hist(dataset, "Age Histogram for " + class_text)
+
     return (
         survived_chart,
         title_chart,
         hist_age
     )
 
-################################################################################
-#                          - END OF HELPER FUNCTIONS -                         #
-################################################################################
+########################################################################################################################
+#                               > MAIN ROUTE
 
-
-################################################################################
-#                               > MAIN ROUTE                                   #
-################################################################################
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def chart():
-    selected_class = request.form.get('dropdown-select')
 
-    if selected_class == 0 or selected_class == None:
-        survived_chart, title_chart, hist_age = redraw(1)
-    else:
-        survived_chart, title_chart, hist_age = redraw(selected_class)
+    selected_class = request.form.get('dropdown-select')
+    if selected_class is None:
+        selected_class = 0
+    survived_chart, title_chart, hist_age = redraw(selected_class)
 
     script_survived_chart, div_survived_chart = components(survived_chart)
     script_title_chart, div_title_chart = components(title_chart)
@@ -121,17 +115,15 @@ def chart():
         selected_class=selected_class
     )
 
-################################################################################
-#                            - END OF MAIN ROUTE -                             #
-################################################################################
+########################################################################################################################
+#                        > CHART GENERATION FUNCTIONS
 
+def survived_bar_chart(dataset, title, cpalette=None):
 
-################################################################################
-#                        > CHART GENERATION FUNCTIONS                          #
-################################################################################
+    if cpalette is None:
+        cpalette = palette[1:3]
 
-def survived_bar_chart(dataset, pass_class, cpalette=palette[1:3]):
-    surv_data = dataset[dataset['Pclass'] == int(pass_class)]
+    surv_data = dataset
     surv_possibilities = list(surv_data['Survived'].value_counts().index)
     surv_values = list(surv_data['Survived'].value_counts().values)
     surv_possibilities_text = ['Did not Survive', 'Survived']
@@ -146,9 +138,11 @@ def survived_bar_chart(dataset, pass_class, cpalette=palette[1:3]):
         tooltips=[('Survived?', '@possibilities_txt'), ('Count', '@values')]
     )
     
-    p = figure(tools=[hover_tool], plot_height=400, title='Did/Did not Survive for Current Class')
+    p = figure(tools=[hover_tool], plot_height=400, title=title)
     p.vbar(x='possibilities', top='values', source=source, width=0.9,
-           fill_color=factor_cmap('possibilities_txt', palette=palette_generator(len(source.data['possibilities_txt']), cpalette), factors=source.data['possibilities_txt']))
+           fill_color=factor_cmap('possibilities_txt',
+                                  palette=palette_generator(len(source.data['possibilities_txt']), cpalette),
+                                  factors=source.data['possibilities_txt']))
     
     plot_styler(p)
     p.xaxis.ticker = source.data['possibilities']
@@ -158,8 +152,10 @@ def survived_bar_chart(dataset, pass_class, cpalette=palette[1:3]):
     return p
 
 
-def class_titles_bar_chart(dataset, pass_class, cpalette=palette):
-    ttl_data = dataset[dataset['Pclass'] == int(pass_class)]
+def class_titles_bar_chart(dataset, title, cpalette=None):
+    if cpalette is None:
+        cpalette = palette
+    ttl_data = dataset
     title_possibilities = list(ttl_data['Title'].value_counts().index)
     title_values = list(ttl_data['Title'].value_counts().values)
     int_possibilities = np.arange(len(title_possibilities))
@@ -178,9 +174,10 @@ def class_titles_bar_chart(dataset, pass_class, cpalette=palette):
     for val1, val2 in zip(source.data['titles_int'], source.data['titles']):
         chart_labels.update({ int(val1): str(val2) })
         
-    p = figure(tools=[hover_tool], plot_height=300, title='Titles for Current Class')
+    p = figure(tools=[hover_tool], plot_height=300, title=title)
     p.vbar(x='titles_int', top='values', source=source, width=0.9,
-           fill_color=factor_cmap('titles', palette=palette_generator(len(source.data['titles']), cpalette), factors=source.data['titles']))
+           fill_color=factor_cmap('titles', palette=palette_generator(len(source.data['titles']), cpalette),
+                                  factors=source.data['titles']))
     
     plot_styler(p)
     p.xaxis.ticker = source.data['titles_int']
@@ -191,8 +188,9 @@ def class_titles_bar_chart(dataset, pass_class, cpalette=palette):
     return p
 
 
-def age_hist(dataset, pass_class, color=palette[1]):
-    hist, edges = np.histogram(dataset[dataset['Pclass'] == int(pass_class)]['Age'].fillna(df['Age'].mean()), bins=25)
+def age_hist(dataset, title, color=palette[1]):
+
+    hist, edges = np.histogram(dataset['Age'].fillna(df['Age'].mean()), bins=25)
     
     source = ColumnDataSource({
         'hist': hist,
@@ -205,7 +203,7 @@ def age_hist(dataset, pass_class, color=palette[1]):
         mode='vline'
     )
     
-    p = figure(plot_height=400, title='Age Histogram', tools=[hover_tool])
+    p = figure(plot_height=400, title=title, tools=[hover_tool])
     p.quad(top='hist', bottom=0, left='edges_left', right='edges_right', source=source,
             fill_color=color, line_color='black')
 
@@ -213,12 +211,6 @@ def age_hist(dataset, pass_class, color=palette[1]):
     p.sizing_mode = 'scale_width'
 
     return p
-
-################################################################################
-#                    - END OF CHART GENERATION FUNCTIONS -                     #
-################################################################################
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- readme.md
+++ readme.md
@@ -9,3 +9,10 @@ Numpy, Pandas, Bokeh, and Flask are required:
 ```bash
 pip install numpy pandas bokeh==1.2.0 flask
 ```
+
+# Changes from original version
+Ver 1.1: Nemecsek
+* All external JS/CSS are now local;
+* Added "All classes" selection;
+* More readable dashboard;
+* Some minimal rename/refactor.
